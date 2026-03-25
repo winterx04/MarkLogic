b705fab996f55a2b8581bfecb9e9479002a56d5a
winterx04 <victoriafooyihui@gmail.com>
2026-03-25 13:43:13 +0800

    ssl

--- app.py
+++ app.py
@@ -41,6 +41,7 @@ app.config['MAIL_SERVER'] = os.getenv('MAIL_SERVER')
 #app.config['MAIL_SERVER'] = 
 app.config['MAIL_PORT'] = int(os.getenv('MAIL_PORT', 587))
 app.config['MAIL_USE_TLS'] = os.getenv('MAIL_USE_TLS') == 'False'
+app.config['MAIL_USE_SSL'] = False
 app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
 #app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
 
