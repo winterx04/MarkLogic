8ec019c00b6da747baa03a96555c2132abb2e06b
winterx04 <victoriafooyihui@gmail.com>
2026-03-25 12:07:13 +0800

    -

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ app.config['EXACT_MATCH_THRESHOLD'] = 0.05
 app.config['MAIL_SERVER'] = os.getenv('MAIL_SERVER')
 #app.config['MAIL_SERVER'] = 
 app.config['MAIL_PORT'] = int(os.getenv('MAIL_PORT', 587))
-#app.config['MAIL_USE_TLS'] = os.getenv('MAIL_USE_TLS') == 'True'
+app.config['MAIL_USE_TLS'] = os.getenv('MAIL_USE_TLS') == 'True'
 app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
 #app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
 
